9c8848d439183c45e56cb4d9b7c4568ab72075fb
Oportugall <lemgruberportugalleonardo@gmail.com>
2026-07-02 22:44:12 -0300

    Testes de integração 2

--- API-dio-bank/src/tests/integration/conftest.py
+++ API-dio-bank/src/tests/integration/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from src.app import create_app, db
+from src.app import create_app, db, User, Role
 
-@pytest.fixture()
+@pytest.fixture
 def app():
     app = create_app(
         {
@@ -13,7 +13,22 @@ def app():
     with app.app_context():
         db.create_all()
         yield app
+        db.drop_all()
 
-@pytest.fixture()
+@pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def access_token(client):
+    role = Role(name='admin')
+    db.session.add(role)
+    db.session.commit()
+
+    user = User(username ='John-Doe', password='test', role_id=role.id)
+    db.session.add(user)
+    db.session.commit()
+
+    response = client.post("/auth/login", json={'username': user.username, "password": user.password})
+    return response.json['access_token']
+
--- API-dio-bank/src/tests/integration/controllers/test_user.py
+++ API-dio-bank/src/tests/integration/controllers/test_user.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-
+from sqlalchemy import func
 from src.app import User, Role, db
 
 def test_get_user_success(client):
@@ -28,3 +28,30 @@ def test_get_user_not_found(client):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
 
+def test_create_user(client, access_token):
+    role_id = db.session.execute(db.select(Role.id).where(Role.name == "admin")).scalar()
+    payload = {'username': 'user2', 'password': 'user2', 'role_id': role_id}
+
+    response = client.post(f'/users/', json = payload, headers={"Authorization": f'Bearer {access_token}'})
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json == {"message": "User created!"}
+    assert db.session.execute(db.select(func.count(User.id))).scalar() == 2
+
+def test_list_users(client, access_token):
+    user = db.session.execute(db.select(User).where(User.username == "John-Doe")).scalar()
+    response = client.post("/auth/login", json={'username': user.username, "password": user.password})
+    access_token = response.json['access_token']
+
+    response = client.get(f'/users/', headers={"Authorization": f'Bearer {access_token}'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == {"users": [
+        {   "id": user.id,
+            "username": user.username,
+            "role": {
+                "id": user.role.id,
+                "Name": user.role.name,
+            },
+        }
+    ]}
